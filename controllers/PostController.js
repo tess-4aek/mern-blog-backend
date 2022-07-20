6292c3dd8db33a0bca7ab2d20c3c0fd62a8b002b
tess-4aek <56252678+tess-4aek@users.noreply.github.com>
2022-07-20 12:46:53 +0300

    add CRUD for posts

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,7 +1,123 @@
-export const create = (req, res) => {
-    try {
+import PostModel from '../models/Post.js';
 
+export const getAll = async(req, res) => {
+    try {
+        const posts = await PostModel.find().populate('user').exec();
+        res.json(posts);
     } catch (e) {
         console.log(e);
+        res.status(500).json({
+            message: 'Не вдалося отримати пости'
+        })
+    }
+}
+
+export const getOne = async(req, res) => {
+    try {
+        const postId = req.params.id;
+        PostModel.findOneAndUpdate({
+                _id: postId
+            }, {
+                $inc: { viewsCount: 1 }
+            }, {
+                returnDocument: 'after'
+            },
+            (e, doc) => {
+                if (e) {
+                    console.log(e);
+                    return res.status(500).json({
+                        message: 'Не вдалося отримати пост'
+                    })
+                }
+
+                if (!doc) {
+                    return res.status(404).json({
+                        message: 'Статя не знайдена'
+                    })
+                }
+
+                res.json(doc);
+            });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Не вдалося отримати пост'
+        })
+    }
+}
+
+export const remove = async(req, res) => {
+    try {
+        const postId = req.params.id;
+        PostModel.findOneAndDelete({
+            _id: postId
+        }, (e, doc) => {
+            if (e) {
+                console.log(e);
+                return res.status(500).json({
+                    message: 'Не вдалося видалити пост'
+                })
+            }
+
+            if (!doc) {
+                return res.status(404).json({
+                    message: 'Статя не знайдена'
+                })
+            }
+
+            res.json({
+                success: true
+            })
+        });
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Не вдалося отримати пост'
+        })
+    }
+}
+
+export const create = async(req, res) => {
+    try {
+        const doc = new PostModel({
+            title: req.body.title,
+            text: req.body.text,
+            tags: req.body.tags,
+            imageUrl: req.body.imageUrl,
+            user: req.userId
+        });
+
+        const post = await doc.save();
+
+        res.json(post);
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Не вдалося створити пост'
+        })
+    }
+}
+
+export const update = async(req, res) => {
+    try {
+        const postId = req.params.id;
+        await PostModel.updateOne({
+            _id: postId
+        }, {
+            title: req.body.title,
+            text: req.body.text,
+            tags: req.body.tags,
+            imageUrl: req.body.imageUrl,
+            user: req.userId
+        });
+
+        res.json({
+            success: true
+        })
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'Не вдалося обновити пост'
+        })
     }
 }
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { registerValidation, loginValidation } from './validations.js';
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
 import * as UserController from './controllers/UserController.js';
+import * as PostController from './controllers/PostController.js';
 
 mongoose.connect('mongodb+srv://admin:admin@cluster0.vpsqlrr.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('Database connected'))
@@ -16,6 +17,13 @@ app.use(express.json());
 app.post('/auth/login', loginValidation, UserController.login);
 app.post('/auth/register', registerValidation, UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.get('/posts', PostController.getAll);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.get('/posts/:id', PostController.getOne);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth, PostController.update);
+
 
 app.listen(PORT, (e) => {
     if (e) {
